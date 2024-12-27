338128d3519f1af962a2eead67df19944c79f444
Denis Luciano <denis.lopes3012@gmail.com>
2024-12-27 14:59:32 -0300

    Ajuste no checkinController

--- controllers/checkinController.js
+++ controllers/checkinController.js
@@ -2,15 +2,13 @@ const moment = require('moment-timezone');
 const Checkin = require('../models/checkin');
 const User = require('../models/user');
 
-const processCheckIn = async (client, message, userId, userName, activitySettings, activityId, category, dateUTC, isOverdue) => {
+const processCheckIn = async (client, message, userId, userName, challenge, category, dateUTC, isOverdue) => {
     // Converte a data UTC para o horário do Brasil (BRT)
     const dateBRT = moment(dateUTC).tz('America/Sao_Paulo');
 
     // Ajusta o início e o fim do dia no horário do Brasil (BRT), depois converte para UTC
     const startOfDay = dateBRT.clone().startOf('day').utc().toDate();
     const endOfDay = dateBRT.clone().endOf('day').utc().toDate();
-
-    atividade = activitySettings["atividade"]
 
     // Verifica se o usuário já existe
     const userAlreadyExist = await User.findOne({
@@ -32,7 +30,7 @@ const processCheckIn = async (client, message, userId, userName, activitySetting
     // Verifica se o usuário já fez check-in na mesma atividade, categoria e data
     const alreadyCheckedIn = await Checkin.findOne({
         'userId': userId,
-        'activity': activityId,
+        'challengeId': challenge._id,
         'category': category,
         'date': {
             $gte: startOfDay, // Início do dia em UTC
@@ -47,7 +45,7 @@ const processCheckIn = async (client, message, userId, userName, activitySetting
     if (alreadyCheckedIn) {
         client.sendMessage(
             message.from,
-            `⚠️ ${userName}, você *já fez* um check-in para atividade *${atividade}* na categoria *${category}* em *${formatedDateBRT}*.`
+            `⚠️ ${userName}, você *já fez* um check-in para atividade *${challenge.activity}* na categoria *${category}* em *${formatedDateBRT}*.`
         );
         return;
     }
@@ -55,7 +53,7 @@ const processCheckIn = async (client, message, userId, userName, activitySetting
     // Cria um novo check-in
     const newCheckIn = new Checkin({
         'userId': userId,
-        'activity': activityId,
+        'challengeId': challenge._id,
         'category': category,
         'date': dateUTC, // Armazena a data original em UTC
         'isOverdue': isOverdue
@@ -65,7 +63,7 @@ const processCheckIn = async (client, message, userId, userName, activitySetting
 
     client.sendMessage(
         message.from,
-        `🥳 *Parabéns* ${userName}! Check-in registrado para atividade *${atividade}* na categoria *${category}* na data de *${formatedDateBRT}*!`
+        `🥳 *Parabéns* ${userName}! Check-in registrado para atividade *${challenge.activity}* na categoria *${category}* na data de *${formatedDateBRT}*!`
     );
 };
 
--- models/checkin.js
+++ models/checkin.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const checkinSchema = new mongoose.Schema({
     userId: String,
-    activity: String,
+    challengeId: { // Referência ao Challenge
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Challenge',
+        required: true // Torne obrigatório se cada checkin precisar estar associado a um desafio
+    },
     category: String,
     date: Date,
     isOverdue: Boolean //se enviou com a flag de "ontem"
